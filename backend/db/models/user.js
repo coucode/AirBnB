915842fa9c529b3071e3a8ab000bd237cf499299
Cecilia Ou <cou.code@gmail.com>
2022-07-28 13:42:06 -0700

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -2,13 +2,54 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require('bcryptjs')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    // return an object with only the User instance info safe to save to a JWT
+    toSafeObject(){
+      const { id, username, email } = this
+      return { id, username, email };
+    }
+    // accepts a password strign and returns true if there is a match to the user's hashedPassword
+    // returns false if no match
+    validatePassword(password){
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+    // accepts an id and returns a user with that id
+    static gerCurrentUserById(id){
+      return User.scope('currentUser').findByPk(id);
+    }
+    // accepts an object with credential and password keys
+    // searches for one user with the specified credential
+    // if found, validate the password using the .validatePassword method
+    // if valid, return the user using currentUser scope
+    static async login({ credential, password }){
+      const {Op} = require ('sequelize')
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)){
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+    // accpets an object with a username, email and password
+    // has the password using bcryptjs
+    // create a user and return the user using the currentUser scope
+    static async signup({ username, email, password }){
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username, 
+        email, 
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id)
+    }
+
     static associate(models) {
       // define association here
     }
@@ -20,8 +61,8 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       validate: {
         len: [4, 30],
-        isNotEmail(value){
-          if (validator.isEmail(value)){
+        isNotEmail(value) {
+          if (validator.isEmail(value)) {
             throw new Error("Cannot be an email")
           }
         }
@@ -46,6 +87,19 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'createdAt', 'updatedAt', 'email']
+      }
+    },
+    scope: {
+      currentUser: {
+        attributes: { exclude: ['hashedPassword']}
+      }, 
+      loginUser: {
+        attributes: {}
+      }
+    }
   });
   return User;
 };
