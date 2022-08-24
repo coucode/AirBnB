afb6db7361d0c305897f453dfec2ee439af4e3c5
Cecilia Ou <cou.code@gmail.com>
2022-08-24 13:41:13 -0700

    Frontend - review inital setup complete

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,13 @@
+import { csrfFetch } from "./csrf";
+
+
+const initialState = {}
+
+const reviewReducer = (state = initialState, action) => {
+  switch(action.type){
+    default:
+      return state;
+  }
+}
+
+export default reviewReducer;
