747440a19bbd789c9d6abaeb3901ce1d675ec955
Cecilia Ou <cou.code@gmail.com>
2022-08-26 01:01:15 -0700

    Test

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import './SignUp.css'
 
 function SignupForm() {
   const dispatch = useDispatch();
