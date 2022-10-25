ad8e6e05a02eb6b15a35c125b1bdf715396cbb9b
Cecilia Ou <cou.code@gmail.com>
2022-10-24 21:54:42 -0700

    Added dev links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,9 +1,23 @@
-
-function Footer(){
+import './Footer.css'
+function Footer() {
 
   return (
-    <div>
-      THIS IS THE FOOTER
+    <div className="footer-container">
+      <div className='splash-dev-info'>
+
+        <div className='splash-dev-text'>
+          <p className='splash-dev-inner-text'>Developer:</p>
+          <p className='splash-dev-inner-text' style={{ color: "var(--orange-button-color)", fontSize: '30px' }}>Cecilia Ou</p>
+        </div>
+        <div className='splash-links' >
+          <a href="https://github.com/coucode" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-square-github fa-2xl devLinks"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ceciliasou" target="_blank" rel="noopener noreferrer">
+            <i className="fa-brands fa-linkedin fa-2xl devLinks"></i>
+          </a>
+        </div>
+      </div>
     </div>
   )
 
