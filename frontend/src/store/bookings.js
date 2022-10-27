6c55332fea4c4730848bd2d7de7f5361ed13b17d
Cecilia Ou <cou.code@gmail.com>
2022-10-26 17:05:59 -0700

    Added store for Bookings by Spot

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,10 +1,18 @@
 import { csrfFetch } from "./csrf";
 
 const GET_USER_BOOKINGS = 'bookings/GETUSERBOOKINGS'
+const GET_SPOT_BOOKINGS = 'bookings/GETSPOTBOOKINGS'
 
 const getBookingsByUser = (bookings) => {
   return {
     type: GET_USER_BOOKINGS, 
+    bookings
+  }
+}
+
+const getBookingsBySpot = (bookings) => {
+  return {
+    type: GET_SPOT_BOOKINGS,
     bookings
   }
 }
@@ -14,7 +22,16 @@ export const getUserBookings = () => async (dispatch) => {
 
   if (response.ok) {
     const bookings = await response.json()
-    dispatch(getBookingsByUser)
+    dispatch(getBookingsByUser(bookings))
+  }
+}
+
+export const getSpotBookings = (payload) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${payload.id}/bookings`)
+
+  if (response.ok) {
+    const bookings = await response.json()
+    dispatch(getBookingsBySpot(bookings))
   }
 }
 
@@ -28,6 +45,12 @@ const bookingReducer = (state = initialState, action) => {
         allBookings[booking.id] = booking
       })
       return {...allBookings}
+    case GET_SPOT_BOOKINGS:
+      const spotBookings = {}
+      action.bookings.forEach(booking => {
+        spotBookings[booking.id] = booking
+      })
+      return { ...spotBookings }
     default:
       return state;
   }
