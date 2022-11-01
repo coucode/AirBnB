3647d31e0947b061c11885df8d8eb6115395f094
Cecilia Ou <cou.code@gmail.com>
2022-10-31 23:06:53 -0700

    Added images and links for bookings by user

--- frontend/src/components/BookingsByUser/index.js
+++ frontend/src/components/BookingsByUser/index.js
@@ -1,14 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from 'react-router-dom';
 import { deleteABooking, getUserBookings } from '../../store/bookings';
+import { getAllSpots } from '../../store/spots';
+import './BookingsByUser.css';
 
-
-function BookingsByUser(){
+function BookingsByUser() {
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
 
   useEffect(() => {
-    if (sessionUser){
+    if (sessionUser) {
       dispatch(getUserBookings())
     }
   }, [dispatch, sessionUser])
@@ -16,38 +18,71 @@ function BookingsByUser(){
   let bookings;
   let loading = true;
 
-  if (bookingsObj){
+  if (bookingsObj) {
     bookings = Object.values(bookingsObj)
     loading = false;
   }
 
+  function imageCheck(spot) {
+    if (spot.previewImage) {
+      return spot.previewImage
+    } else {
+      return "https://st.depositphotos.com/1987177/3470/v/450/depositphotos_34700099-stock-illustration-no-photo-available-or-missing.jpg"
+    }
+  }
+
   return (
-    <div>
+    <div className='bookings-by-user-container'>
       {(loading === false) && (bookings.length > 0) ? (
         <>
-        {bookings.map(booking => {
-          return (
-            <div key = {booking.id}>
-              {booking.Spot.name}
-              <br/>
-              - START: {booking.startDate}
-              <br />
-              - END: {booking.endDate}
-              <br />
-              <br />
-              <button onClick={(e)  => {
-                dispatch(deleteABooking(booking.id))
-              }}>Delete Booking</button>
-              <br />
-              <br />
-            </div>
-          )
-        })}
+          {bookings.map(booking => {
+            return (
+              <div key={booking.id} className='single-booking-container'>
+                <NavLink to={`/spots/${booking.spotId}`} className='single-booking-links'>
+                  <div className='single-booking-links'>
+                    <img
+                      src={imageCheck(booking.Spot)}
+                      alt="listing"
+                      style={{ width: 125, height: 125 }}
+                    />
+                    <div className='single-booking-text'>
+                      <p className='single-booking-name'>
+                        {booking?.Spot.name}
+                      </p>
+                      <div className='single-booking-dates'>
+                        <p style={{ fontWeight: '600', paddingRight: '10px' }}>
+                          Check-In:
+                        </p>
+                        <p>
+                          {booking?.startDate}
+                        </p>
+                      </div>
+                      <div className='single-booking-dates'>
+                        <p style={{ fontWeight: '600', paddingRight: '10px' }}>
+                          Check-Out:
+                        </p>
+                        <p>
+                          {booking?.endDate}
+                        </p>
+                      </div>
+
+                    </div>
+                  </div>
+                </NavLink>
+
+                <button onClick={(e) => {
+                  dispatch(deleteABooking(booking.id))
+                }}>Delete Booking</button>
+                <br />
+                <br />
+              </div>
+            )
+          })}
         </>
 
-      ): (
-          <div>
-          </div>
+      ) : (
+        <div>
+        </div>
       )}
     </div>
   )
