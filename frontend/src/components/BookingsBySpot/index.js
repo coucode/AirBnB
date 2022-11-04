15e0a580bb30f680422304c2b11d7c4be3c24968
Cecilia Ou <cou.code@gmail.com>
2022-11-04 13:30:17 -0700

    Added comments to bookings by spot component

--- frontend/src/components/BookingsBySpot/index.js
+++ frontend/src/components/BookingsBySpot/index.js
@@ -1,21 +1,20 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
-
-import { deleteABooking, getSpotBookings } from '../../store/bookings';
+import { getSpotBookings } from '../../store/bookings';
 import './BookingsBySpot.css'
 
 function BookingsBySpot() {
   const dispatch = useDispatch()
   const { id } = useParams()
-  const sessionUser = useSelector(state => state.session.user)
   let loading = true;
 
-
+  // Gets all of the bookings for the spot specified in the URL
   useEffect(() => {
     dispatch(getSpotBookings(id))
   }, [dispatch, id])
 
+  // Obtains the bookings from state, creates an array with that information
   let bookings;
   const bookingsObj = useSelector(state => state.bookings)
   if (bookingsObj) {
@@ -23,10 +22,12 @@ function BookingsBySpot() {
     loading = false;
   }
 
+  // Rerenders the component while state is loading
   if (!bookingsObj) return null
   if (!bookings) return null
 
-
+  // Sorts the bookings into past and future arrays so they can be displayed separately. 
+  // Created a variable for today's date, and change the string values to date values in order to compare
   let currentDate = new Date()
   let pastBookings = []
   let futureBookings = []
@@ -42,6 +43,7 @@ function BookingsBySpot() {
     })
   }
 
+  // Variable that is returned when there are no bookings
   let noBookings = (
     <div className='bookings-by-spot-info-container'>
       <p className='bookings-by-spot-info'>• No Bookings</p>
@@ -56,6 +58,7 @@ function BookingsBySpot() {
           <div className='bookings-by-spot-inner-container'>
             <div className='bookings-current-container'>
               <p className='bookings-date-headers'>Current and Upcoming Bookings</p>
+              {/* Function checks if the array has no values, it will return a no bookings message, else map through the array and display information */}
               {(futureBookings?.length === 0) ? noBookings : futureBookings.map(booking => {
                 return (
                   <div key={booking.id} className='bookings-by-spot-info-container'>
@@ -68,6 +71,7 @@ function BookingsBySpot() {
             </div>
             <div>
               <p className='bookings-date-headers'>Past Bookings</p>
+              {/* Function checks if the array has no values, it will return a no bookings message, else map through the array and display information */}
               {(pastBookings?.length === 0) ? noBookings : pastBookings.map(booking => {
                 return (
                   <div key={booking.id} className='bookings-by-spot-info-container'>
