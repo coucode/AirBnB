ecdcd06f87329dad9f42102cb5dded0d713d8fa1
Cecilia Ou <cou.code@gmail.com>
2022-11-05 14:58:21 -0700

    Created basic component with calendar package

--- frontend/src/components/CreateBooking/index.js
+++ frontend/src/components/CreateBooking/index.js
@@ -2,26 +2,45 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { createABooking, getSpotBookings } from '../../store/bookings';
+import { Calendar } from "react-multi-date-picker"
 
 function CreateBookingForm() {
+  const [values, setValues] = useState([
+    new Date()
+  ])
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user)
-  let minDate = new Date()
 
-  const [startDate, setStartDate] = useState(minDate.toJSON().split('T')[0])
+  const [startDate, setStartDate] = useState('')
   const [endDate, setEndDate] = useState('')
   const [hasSubmitted, setHasSubmitted] = useState(false)
   const { id } = useParams()
 
+  useEffect(() => {
+    if (values.length === 2) {
+      let start = new Date(values[0].year, values[0].month, values[0].day) 
+      let end = new Date(values[1].year, values[1].month, values[1].day) 
+
+      setStartDate(start)
+      setEndDate(end)
+    }
+  }, [values])
+
+  console.log("VALUES========", values)
+  console.log("VALUES 1========", values[0])
+  console.log("VALUES 2========", values[1])
+
+  console.log("START======", startDate)
+  console.log("END=========", endDate)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     setHasSubmitted(true)
 
     const payload = {
-      startDate, 
-      endDate, 
-      spotId: id, 
+      startDate,
+      endDate,
+      spotId: id,
       userId: sessionUser.id
     }
 
@@ -32,19 +51,12 @@ function CreateBookingForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="date"
-        min = {minDate}
-        value= {startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      >
-      </input>
-      <input
-        type="date"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-      >
-      </input>
+      < Calendar
+        value={values}
+        onChange={setValues}
+        range
+        rangeHover
+      />
       <div>
         <button> Submit </button>
       </div>
